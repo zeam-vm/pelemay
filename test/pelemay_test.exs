64fe1c8a8be3bf76257ce3bb82cf2d35fcd259b4
Susumu Yamazaki <zacky1972@gmail.com>
2019-09-19 20:40:24 +0900

    towards fix #36

--- test/pelemay_test.exs
+++ test/pelemay_test.exs
@@ -1,4 +1,20 @@
 defmodule PelemayTest do
   use ExUnit.Case, async: true
   doctest Pelemay
+
+  test "Basic functional testing for Pelemay" do
+  	result = 1..4 |> Enum.to_list |> PelemayTest.Sample.map_square
+  	assert result == [1, 4, 9, 16]
+  end
+end
+
+defmodule PelemayTest.Sample do
+  require Pelemay
+  import Pelemay
+
+  defpelemay do
+    def map_square list do
+      list |> Enum.map(& &1 * &1)
+    end
+  end
 end
