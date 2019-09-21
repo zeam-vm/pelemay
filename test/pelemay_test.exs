71471279be52abca7f3fad473fd1836679c8737e
Susumu Yamazaki <zacky1972@gmail.com>
2019-09-22 06:11:40 +0900

    apply mix format

--- test/pelemay_test.exs
+++ test/pelemay_test.exs
@@ -3,8 +3,8 @@ defmodule PelemayTest do
   doctest Pelemay
 
   test "Basic functional testing for Pelemay" do
-  	result = 1..4 |> Enum.to_list |> PelemayTest.Sample.map_square
-  	assert result == [1, 4, 9, 16]
+    result = 1..4 |> Enum.to_list() |> PelemayTest.Sample.map_square()
+    assert result == [1, 4, 9, 16]
   end
 end
 
@@ -13,8 +13,8 @@ defmodule PelemayTest.Sample do
   import Pelemay
 
   defpelemay do
-    def map_square list do
-      list |> Enum.map(& &1 * &1)
+    def map_square(list) do
+      list |> Enum.map(&(&1 * &1))
     end
   end
 end
