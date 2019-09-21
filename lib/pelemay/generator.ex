f4e01fe10ab0d7df33250e29051162bac7f8a463
Susumu Yamazaki <zacky1972@gmail.com>
2019-09-21 12:19:16 +0900

    add some doctests to Pelemay.Generator

--- lib/pelemay/generator.ex
+++ lib/pelemay/generator.ex
@@ -1,36 +1,100 @@
 defmodule Pelemay.Generator do
   alias Pelemay.Generator.Interface
   alias Pelemay.Generator.Native
-  alias Pelemay.Generator.Makefile
+  alias Pelemay.Generator.Builder
 
+  @doc """
+
+  ## Examples
+
+    iex> Pelemay.Generator.module_replaced_non(:"Elixir.Module")
+    "ElixirModule"
+
+  """
   def module_replaced_non(module) do
     module |> Atom.to_string() |> String.replace(".", "")
   end
 
+  @doc """
+
+  ## Examples
+
+    iex> Pelemay.Generator.module_replaced_underscore(:"Elixir.Module")
+    "Elixir_Module"
+    
+  """
   def module_replaced_underscore(module) do
     module |> Atom.to_string() |> String.replace(".", "_")
   end
 
+  @doc """
+
+  ## Examples
+
+    iex> Pelemay.Generator.nif_module(:"Elixir.Module")
+    "PelemayNifElixirModule"
+
+  """
   def nif_module(module) do
     "PelemayNif#{module_replaced_non(module)}"
   end
 
+  @doc """
+
+  ## Examples
+
+    iex> Pelemay.Generator.elixir_nif_module(:"Elixir.Module")
+    "Elixir.PelemayNifElixirModule"
+    
+  """
   def elixir_nif_module(module) do
     "Elixir.PelemayNif#{module_replaced_non(module)}"
   end
 
+  @doc """
+
+  ## Examples
+
+    iex> Pelemay.Generator.module_downcase_non(:"Elixir.Module")
+    "elixirmodule"
+    
+  """
   def module_downcase_non(module) do
     module |> module_replaced_non() |> String.downcase()
   end
 
+  @doc """
+
+  ## Examples
+
+    iex> Pelemay.Generator.module_downcase_underscore(:"Elixir.Module")
+    "elixir_module"
+    
+  """
   def module_downcase_underscore(module) do
     module |> module_replaced_underscore() |> String.downcase()
   end
 
+  @doc """
+
+  ## Examples
+
+    iex> Pelemay.Generator.libnif_name(:"Elixir.Module")
+    "libnifelixirmodule"
+    
+  """
   def libnif_name(module) do
     "libnif#{module_downcase_non(module)}"
   end
 
+  @doc """
+
+  ## Examples
+
+    iex> Pelemay.Generator.libnif_priv_name(:"Elixir.Module")
+    "priv/libnifelixirmodule"
+    
+  """
   def libnif_priv_name(module) do
     "priv/libnif#{module_downcase_non(module)}"
   end
@@ -57,6 +121,5 @@ defmodule Pelemay.Generator do
 
     Interface.generate(module)
     Native.generate(module)
-    Makefile.generate(module)
   end
 end
