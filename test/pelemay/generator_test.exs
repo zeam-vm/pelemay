4df47a9a8ea6b2b6405ba2b7943216756c4756f3
HISA WAY <z8mcb008@gmail.com>
2019-09-27 19:07:09 +0900

    Add check for doctests.

--- test/pelemay/generator_test.exs
+++ test/pelemay/generator_test.exs
@@ -1,4 +1,7 @@
 defmodule Pelemay.GeneratorTest do
   use ExUnit.Case
   doctest Pelemay.Generator
+  doctest Pelemay.Generator.Interface
+  doctest Pelemay.Generator.Native
+  doctest Pelemay.Generator.Builder
 end
