6f768da8eea6ce4222fba6a1164c2a6aec1e3c33
Susumu Yamazaki <zacky1972@gmail.com>
2019-10-02 22:29:22 +0900

    compiler uses in order , clang and gcc

--- lib/pelemay/generator/builder.ex
+++ lib/pelemay/generator/builder.ex
@@ -1,7 +1,8 @@
 defmodule Pelemay.Generator.Builder do
   alias Pelemay.Generator
 
-  @cc "gcc"
+  @clang "clang"
+  @gcc "gcc"
   @cflags ["-Ofast", "-g", "-ansi", "-pedantic", "-femit-all-decls"]
   @cflags_includes ["-I/usr/local/include", "-I/usr/include", "-L/usr/local/lib", "-L/usr/lib"]
   @cflags_after ["-std=c11", "-Wno-unused-function"]
@@ -10,8 +11,22 @@ defmodule Pelemay.Generator.Builder do
   @ldflags_non_windows ["-dynamiclib", "-undefined", "dynamic_lookup"]
 
   def generate(module) do
-    if is_nil(System.find_executable(@cc)) do
-      raise CompileError, message: "#{@cc} is not installed."
+  	cc = System.get_env("CC")
+
+    cc = if is_nil(System.find_executable(cc)) do 
+      @clang 
+    else
+      cc
+    end
+
+    cc = if is_nil(System.find_executable(cc)) do 
+      @gcc
+    else
+      cc
+    end
+
+    if is_nil(System.find_executable(cc)) do
+      raise CompileError, message: "#{cc} is not installed."
     end
 
     cflags_t = @cflags ++ ["-I#{erlang_include_path()}"] ++ @cflags_includes ++ @cflags_after
@@ -32,7 +47,7 @@ defmodule Pelemay.Generator.Builder do
     options =
       cflags ++ ["-shared"] ++ ldflags ++ ["-o", Generator.libso(module), Generator.libc(module)]
 
-    {_result, 0} = System.cmd(@cc, options)
+    {_result, 0} = System.cmd(cc, options)
   end
 
   def erlang_include_path() do
