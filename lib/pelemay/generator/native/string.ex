2f6e0ed001c26b68339cb044033fc99005cf24ea
HISA WAY <z8mcb008@gmail.com>
2020-02-09 20:43:07 +0900

    Add hooks for native-string function

---
+++ lib/pelemay/generator/native/string.ex
@@ -0,0 +1,6 @@
+defmodule Pelemay.Generator.Native.String do
+  alias Pelemay.Generator.Native.Util, as: Util
+
+  def replace(%{nif_name: nif_name, args: args, operators: operators}) do
+  end
+end
