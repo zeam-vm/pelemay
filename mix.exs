179529065e26c21cabc40fd5c823a7c043b57900
Susumu Yamazaki <zacky1972@gmail.com>
2020-04-02 15:31:47 +0900

    for v0.0.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pelemay.MixProject do
   def project do
     [
       app: :pelemay,
-      version: "0.0.6",
+      version: "0.0.7",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
