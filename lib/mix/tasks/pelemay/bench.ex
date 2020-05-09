2c3f9318e55ef2e266264a199f2c553dc177cce4
Susumu Yamazaki <zacky1972@gmail.com>
2020-05-09 11:50:12 +0900

    apply mix format

--- lib/mix/tasks/pelemay/bench.ex
+++ lib/mix/tasks/pelemay/bench.ex
@@ -15,10 +15,13 @@ defmodule Mix.Tasks.Pelemay.Bench do
   @impl true
   def run(args) do
     Mix.Project.get!()
+
     args
-    |> Enum.map(& "Elixir.#{&1}")
-    |> Enum.map(& String.to_atom(&1))
-    |> Enum.map(& Pelemay.Generator.nif_module(&1))
-    |> Enum.each(& Mix.Shell.cmd("mix run -e \"#{&1}.bench() |> IO.inspect()\"", fn x -> IO.puts(x) end))
+    |> Enum.map(&"Elixir.#{&1}")
+    |> Enum.map(&String.to_atom(&1))
+    |> Enum.map(&Pelemay.Generator.nif_module(&1))
+    |> Enum.each(
+      &Mix.Shell.cmd("mix run -e \"#{&1}.bench() |> IO.inspect()\"", fn x -> IO.puts(x) end)
+    )
   end
 end
